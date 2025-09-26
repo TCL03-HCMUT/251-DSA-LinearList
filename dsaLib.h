52068e2f4dbe17969c096534bd6af19a7d785233
TCL03 <khoanguyenanh513@gmail.com>
2025-09-26 13:29:24 +0700

    implemented singly linked list (untested)

--- dsaLib.h
+++ dsaLib.h
@@ -113,9 +113,15 @@ public:
     }
     virtual LinearList<T> *clone(int sidx = 0, int eidx = -1)
     {
-        LinearList<T> *pL = new ArrayList<T>;
 
         // FIXME: missing constructor
+        if (eidx < 0)
+            eidx = nE;
+        LinearList<T> *pL = new ArrayList<T>(eidx - sidx);
+        for (int i = sidx; i < eidx; ++i)
+        {
+            pL->insert(pD[i], i - sidx);
+        }
         return pL;
     }
 
@@ -269,6 +275,333 @@ public:
         for (T *p = pD, *pE = pD + nE; p != pE; p++)
         {
             action(*p);
+        }
+    }
+};
+
+// TODO: Linked List implementation of LinearList
+template <class T>
+class L1List : public LinearList<T>
+{
+protected:
+    struct Node
+    {
+        T data;
+        Node *pNext;
+        Node(const T &val, Node *p = nullptr) : data(val), pNext(p) {}
+        Node(T &&val, Node *p = nullptr) : data(std::move(val)), pNext(p) {}
+    };
+
+    Node *pHead, *pTail;
+    int nE;
+
+public:
+    L1List() : nE(0), pHead(nullptr), pTail(nullptr) {}
+    ~L1List() { clear(); }
+
+    void resize(int N)
+    {
+        return;
+    }
+
+    virtual bool isEmpty() { return !nE; }
+    virtual int getSize() { return nE; }
+    virtual void clear()
+    {
+        while (!pHead)
+        {
+            Node *p = pHead;
+            pHead = pHead->pNext;
+            delete p;
+        }
+        nE = 0;
+        pTail = nullptr;
+    }
+    virtual LinearList<T> *clone(int sidx = 0, int eidx = -1)
+    {
+        if (eidx < 0)
+            eidx = nE;
+        LinearList<T> *pL = new L1List<T>;
+        Node *p = this->pHead, **pNL = &((L1List<T>*)pL->pHead);
+        eidx -= sidx;
+        pL->nE = eidx;
+        while (sidx != 0 && p != nullptr)
+        {
+            p = p->next;
+            --sidx;
+        }
+
+        while (p && eidx)
+        {
+            --eidx;
+            *pNL = new Node(p->data);
+            // pL->pTail = *pNL;
+            pNL = &(*pNL->pNext);
+            p = p->next;
+        }
+
+        // Get the tail
+        // risky option
+        pL->pTail = (Node *)((char *)pNL - sizeof(T)); //  uses data alignment
+
+        // // safe option
+        // Node *pT = pL->pHead;
+        // while (pT->pNext) pT = pT->pNext;
+        // pL->pTail = pT;
+
+        return pL;
+    }
+
+    virtual T &operator[](int index) // precondition: index must be valid
+    {
+        // if (index < 0 || index >= nE)
+        //     throw DSA_Exception{911, "ArrayList::operator[]: Invalid index"};
+        // return pD[index];
+        Node *pT = pHead;
+        while (pT && index)
+        {
+            pT = pT->pNext;
+            --index;
+        }
+        if (pT)
+            return pT->data;
+        else
+            throw out_of_range("Invalid index");
+    }
+
+    virtual bool insert(const T &value, int index)
+    {
+        Node **pp = &pHead;
+        while (*pp && index)
+        {
+            --index;
+            pp = &((*pp)->pNext);
+        }
+        (*pp) = new Node(value, *pp);
+        ++nE;
+        if (pTail->pNext)
+            pTail = pTail->pNext;
+        
+        return true;
+    }
+    virtual bool insert(T &&value, int index)
+    {
+        Node **pp = &pHead;
+        while (*pp && index)
+        {
+            --index;
+            pp = &((*pp)->pNext);
+        }
+        (*pp) = new Node(std::move(value), *pp);
+        ++nE;
+        if (pTail->pNext)
+            pTail = pTail->pNext;
+        return true;
+    }
+    // move an entire object
+    virtual bool append(const T &value)
+    {
+        if (pHead)
+        {
+            pTail->pNext = new Node(value);
+            pTail = pTail->pNext;
+        }
+        else
+        {
+            pHead = pTail = new Node(value);
+        }
+        ++nE;
+        return true;
+    }
+    virtual bool append(T &&value)
+    {
+        if (pHead)
+        {
+            pTail->pNext = new Node(std::move(value));
+            pTail = pTail->pNext;
+        }
+        else
+        {
+            pHead = pTail = new Node(std::move(value));
+        }
+        ++nE;
+        return true;
+    }
+
+    virtual bool removeIdx(int index)
+    {
+        if (!nE)
+            return false;
+        Node **pp = &pHead;
+        while (*pp && index)
+        {
+            --index;
+            pp = &((*pp)->pNext);
+        }
+        if (*pp)
+        {
+            --nE;
+            Node *p = *pp;
+            if (p->pNext == nullptr)
+            {
+                pTail = (Node *)((char *)pp - sizeof(T)); // FIXME: VERY RISKY
+            }
+            *pp = (*pp)->pNext;
+            delete p;
+        }
+        else
+            return false;
+
+        return true;
+    }
+    virtual bool remove(const T &k, function<bool(const T &, const T &)> eq = [](const T &val, const T &key) -> bool
+                        { return val == key; })
+    {
+        if (!nE)
+            return false;
+        Node **pp = &pHead;
+        while (*pp)
+        {
+            while (eq((*pp)->data, k)) // remove it
+            {
+                Node *p = *pp;
+                *pp = (*pp)->pNext;
+                delete p;
+                --nE;
+            }
+            if ((*pp) != nullptr)
+                pp = &((*pp)->pNext);
+            else
+                pTail = (Node *)((char *)pp - sizeof(T));
+        }
+        return true;
+    } // remove an item by a comparison to a key
+
+    virtual T *find(const T &k, function<bool(const T &, const T &)> eq = [](const T &val, const T &key) -> bool
+                    { return val == key; })
+    {
+        for (Node *p = pHead; p != nullptr; p = p->next)
+        {
+            if (eq(p->data, k))
+                return p->data;
+        }
+        return nullptr;
+    }
+
+    virtual int findIdx(const T &k, function<bool(const T &, const T &)> eq = [](const T &val, const T &key) -> bool
+                        { return val == key; })
+    {
+        int index = 0;
+        for (Node *p = pHead; p != nullptr; p = p->pNext)
+        {
+            if (eq(p->data, k)) return index;
+            index++; 
+        }
+        return -1;
+    }
+
+    virtual LinearList<void *> *findAll(const T &k, function<bool(const T &, const T &)> eq = [](const T &val, const T &key) -> bool
+                                        { return val == key; })
+    {
+        LinearList<void *> *pL = new ArrayList<void *>;
+        // traverse([&pL, &k, &eq](T &val)
+        //          { if (eq(val, k)) pL->append((void*)&val); });
+        return pL;
+    }
+
+    virtual void swap(int idxA, int idxB) // suppose indices are valid idxA < idxB
+    {
+        Node **pp1 = &pHead, **pp2 = nullptr;
+        while (idxA)
+        {
+            --idxA;
+            --idxB;
+            pp1 = &((*pp1)->pNext);
+        }
+        if (idxB == 1) // swapping two elements next to each other
+        {
+            Node *pA = *pp1;
+            Node *pB = pA->pNext;
+            Node *pC = pB->pNext;
+
+            *pp1 = pB;
+            pB->pNext = pA;
+            pA->pNext = pC;
+            return;
+        }
+        else
+        {
+            pp2 = &((*pp1)->pNext);
+            while (idxB > 1)
+            {
+                --idxB;
+                pp2 = &((*pp2)->pNext);
+            }
+            Node *pA = *pp1;
+            Node *pA1 = pA->pNext;
+            Node *pB = *pp2;
+            Node *pB1 = pB->pNext;
+
+            (*pp1) = pB;
+            pB->pNext = pA1;
+
+            (*pp2) = pA;
+            pA->pNext = pB1;
+        }
+    }
+    virtual void reverse()
+    {
+        Node *pNL = nullptr;
+        while (pHead)
+        {
+            Node *p = pHead;
+            pHead = pHead->pNext;
+
+            p->pNext = pNL;
+            pNL = p;
+        }
+        pHead = pNL;
+    }
+    virtual LinearList<T> *split(int index)
+    {
+        LinearList<T> *pL = new L1List<T>;
+        Node **pp = &pHead;
+        (L1List<T>*)(pL)->nE = this->nE - index;
+        nE = index;
+        pL->pTail = this->pTail;
+
+        while (index)
+        {
+            --index;
+            pp = &((*pp)->pNext);
+
+        }
+        
+        pL->pHead = *pp;
+        *pp = nullptr;
+        this->pTail = (Node *)((char *)pp - sizeof(T));
+
+        return pL;
+    } // split from some index, the returned list will be the second one while the current list will be reduced to the first
+    virtual void merge(LinearList<T> *pL)
+    {
+        LinearList<T> *p = this;
+        pL->traverse([p](T &val)
+                     { p->append(val); });
+    }
+
+    virtual void reduceSize(int newCap)
+    {
+        return;
+    }
+    virtual void shuffle()
+    {
+    }
+    virtual void traverse(function<void(T &)> action)
+    {
+        for (Node *p = pHead; p != nullptr; p = p->pNext)
+        {
+            action(p->data);
         }
     }
 };
